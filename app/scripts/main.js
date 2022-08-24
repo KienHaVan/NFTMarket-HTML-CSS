ea235f5b2dc1f7a384202da4f46710ee845f9b80
KienHaVan <hakien351998@gmail.com>
2022-08-24 13:51:43 +0700

    update 24/8

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const headerToggle = $(".header-toggle");
 const headerMenu = $(".header-menu");
-console.log(headerToggle, headerMenu);
+const $jq = jQuery.noConflict();
 
 // header
 
@@ -16,6 +16,72 @@ window.addEventListener("click", (e) => {
 });
 
 // latest
-$(document).ready(function () {
-    $(".latest-list").slick();
+// $(document).ready(() => {
+
+// });
+$jq(".latest-list").slick({
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 3,
+    // slidesToScroll: 1,
+    arrows: true,
+    prevArrow:
+        "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+        "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+        {
+            breakpoint: 1280,
+            settings: {
+                centerMode: true,
+                slidesToShow: 2,
+            },
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                centerMode: false,
+                slidesToShow: 1,
+            },
+        },
+    ],
 });
+// HOTTEST LEFT
+$jq(".hottest-left-list").slick({
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 1,
+    arrows: false,
+    centerPadding: "60px",
+    prevArrow:
+        "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:
+        "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                centerMode: true,
+                slidesToShow: 2,
+                arrows: true,
+            },
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                centerMode: false,
+                slidesToShow: 1,
+                arrows: true,
+            },
+        },
+    ],
+});
+
+// POPULAR
+// $jq(".popular-content").slick({
+//     infinite: true,
+//     centerMode: true,
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     arrows: true,
+// });
